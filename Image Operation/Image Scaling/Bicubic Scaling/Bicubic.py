c8e8baa473ca70b1e74f38826d3a93c72a64a577
Nishitha0730 <pitigalapknw.22@uom.lk>
2025-07-22 14:43:19 +0530

    Update Bicubic.py

--- Image Operation/Image Scaling/Bicubic Scaling/Bicubic.py
+++ Image Operation/Image Scaling/Bicubic Scaling/Bicubic.py
@@ -14,3 +14,43 @@ def cubic_kernel(t):
         return a * abs_t3 - 5 * a * abs_t2 + 8 * a * abs_t - 4 * a
     else:
         return 0
+
+def get_pixel(pixels, x, y):
+    height, width, _ = pixels.shape
+    x = min(max(0, x), width - 1)
+    y = min(max(0, y), height - 1)
+    return pixels[y, x]
+
+def bicubic_interpolation(source_img, new_width, new_height):
+    src_width, src_height = source_img.size
+    src_pixels = np.array(source_img, dtype=float)
+
+    target_img = Image.new("RGB", (new_width, new_height))
+    tgt_pixels = target_img.load()
+
+    scale_x = src_width / new_width
+    scale_y = src_height / new_height
+
+    for y in range(new_height):
+        for x in range(new_width):
+            src_x = x * scale_x
+            src_y = y * scale_y
+
+            x_int = int(np.floor(src_x))
+            y_int = int(np.floor(src_y))
+            dx = src_x - x_int
+            dy = src_y - y_int
+
+            interpolated_pixel = np.zeros(3)
+
+            for m in range(-1, 3):
+                for n in range(-1, 3):
+                    pixel = get_pixel(src_pixels, x_int + n, y_int + m)
+                    weight = cubic_kernel(n - dx) * cubic_kernel(m - dy)
+                    interpolated_pixel += pixel * weight
+
+            # Clip pixel values and assign
+            interpolated_pixel = np.clip(interpolated_pixel, 0, 255)
+            tgt_pixels[x, y] = tuple(interpolated_pixel.astype(np.uint8))
+
+    return target_img
