d833205c2ed5aad3a105f5f9581988a698df376b
Nishitha0730 <pitigalapknw.22@uom.lk>
2025-07-23 08:19:42 +0530

    no changes

--- Image Operation/Image Scaling/Bicubic Scaling/main.py
+++ Image Operation/Image Scaling/Bicubic Scaling/main.py
@@ -2,7 +2,7 @@ from PIL import Image
 from Bicubic import bicubic_interpolation
 
 def main():
-    source = Image.open("apple.jpeg").convert("RGB")
+    source = Image.open("apple.jpeg").convert("RGB")  # Convert to grayscale
     resized = bicubic_interpolation(source, 789, 573)
     resized.save("bicubic_output.jpg")
     resized.show()
