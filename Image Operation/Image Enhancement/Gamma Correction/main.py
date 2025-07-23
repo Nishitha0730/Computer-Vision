76cffe112380525d2a1ee66be04f69ceb59944f9
Nishitha0730 <pitigalapknw.22@uom.lk>
2025-07-23 23:02:09 +0530

    save output

--- Image Operation/Image Enhancement/Gamma Correction/main.py
+++ Image Operation/Image Enhancement/Gamma Correction/main.py
@@ -6,11 +6,15 @@ from Gamma_Correction import gamma_correction
 def main():
     img = cv.imread("light_nature.jpg")
 
-    corrected_img = gamma_correction(img, 1.5)
+    corrected_img = gamma_correction(img, 2)
+    corrected_img_rgb = cv.cvtColor(corrected_img, cv.COLOR_BGR2RGB)
 
     fig, ax = plt.subplots(figsize=(12, 8))
-    ax.imshow(cv.cvtColor(corrected_img, cv.COLOR_BGR2RGB))
+    ax.imshow(corrected_img_rgb)
     ax.set_title("Gamma Corrected Image")
+
+    # cv.cvtColor(corrected_img, cv.COLOR_BGR2RGB).save("gamma_corrected_output.jpg")
+    cv.imwrite("gamma_corrected_output_gamma_2.jpg", corrected_img)
     plt.show()
 
 if __name__ == "__main__":
