a72bd67093e9639db6e7734f3e6c5ab903ab26d0
Nishitha0730 <pitigalapknw.22@uom.lk>
2025-07-23 12:58:27 +0530

    comment updated

--- Image Operation/Image Enhancement/Contrast Stretching/main.py
+++ Image Operation/Image Enhancement/Contrast Stretching/main.py
@@ -3,7 +3,7 @@ from Contrast_Stretching import contrast_stretching
 
 
 def main():
-    source = Image.open("Apple_GRAY.jpg").convert("L") # Convert to grayscale if not already
+    source = Image.open("Apple_GRAY.jpg").convert("L") # Converts a color image to 8-bit grayscale
     stretched = contrast_stretching(source)
     stretched.save("contrast_stretched_output.jpg")
     stretched.show()
