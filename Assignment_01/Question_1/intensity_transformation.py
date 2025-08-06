b4707b43dd59a966ae5fb8695c9eb2f082fc486f
Nishitha0730 <pitigalapknw.22@uom.lk>
2025-08-06 22:03:15 +0530

    import image and convert BGR to RGB

--- Assignment_01/Question_1/intensity_transformation.py
+++ Assignment_01/Question_1/intensity_transformation.py
@@ -2,3 +2,7 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
+img = cv.imread(r'F:\SEM 5\Computer Vision\Project\Assignment_01\a1images\emma.jpg')
+rgb_img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+plt.imshow(rgb_img)
+plt.show()
